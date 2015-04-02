a5e9f10d4aec994d036f723c6102ebdb57fceb6e
Leo Schettini <ljsa@cin.ufpe.br>
2015-04-02 20:52:51 -0300

    criação dos models

--- linkme_app/admin.py
+++ linkme_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from models import *
 
 admin.site.register(Link)
-admin.site.register(Source)
+admin.site.register(Target)
--- linkme_app/models.py
+++ linkme_app/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
-class Link (models.Model):
-	id = models.AutoField(primary_key=True)
-	source = models.ForeignKey(Source)
-	target = models.CharField(max_length=30)
-
-	def __unicode__(self):
-		return self.id
-
-class Source (models.Model):
+class Target (models.Model):
 	url = models.CharField(max_length=30)
 
 	def __unicode__(self):
 		return self.url
+
+class Link (models.Model):
+	# id =  models.AutoField(primary_key=True)
+	source = models.CharField(max_length=30)
+	target = models.ForeignKey(Target)
+
+	def __unicode__(self):
+		return self.source
 # Create your models here.
