75f31b3d6b34b20f411e907993109f4245a91f92
Leo Schettini <ljsa@cin.ufpe.br>
2015-04-03 11:58:32 -0300

    front

---
+++ linkme_app/static/js/index.js
@@ -0,0 +1,40 @@
+var inputS = document.getElementById('source');
+var inputT = document.getElementById('target');
+var body = document.getElementById('index');
+
+var lightS = false;
+var lightT = false;
+
+inputS.addEventListener('click', function() {
+
+	lightS = true;
+	inputS.setAttribute('class', 'onclick');
+	inputS.setAttribute('style', 'width: 89px;');
+	inputS.setAttribute('placeholder', 'eg.: lower-url/');
+});
+
+inputT.addEventListener('click', function() {
+
+	lightT = true;
+	inputT.setAttribute('class', 'onclick');
+ 	inputT.setAttribute('style', 'width: 385px;');
+	inputT.setAttribute('placeholder', 'eg.: www.my-ultra-site.com.br/50characters-maximum.html');
+});
+
+body.addEventListener('click', function() {
+	console.log('aae');
+	if (!lightS && inputS.value==""){
+		inputS.setAttribute('class', 'input-container');
+		inputS.setAttribute('style', 'width: 68px;');
+		inputS.setAttribute('placeholder', '<base_dir>');
+	}
+
+	if (!lightT && inputT.value==""){
+		inputT.setAttribute('class', 'input-container');
+		inputT.setAttribute('style', 'width: 74px;');
+		inputT.setAttribute('placeholder', '<target_url>');
+	}
+	//como os dois eventos sao executados qnd o input é ativado, o semaforo so eh apagado aqui
+	lightS = false;
+	lightT = false;
+});
